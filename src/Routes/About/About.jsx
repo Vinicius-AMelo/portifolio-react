5cc0ab3480157b89fae68e8940da7485740c077b
Vinícius Melo <vinicius.apol.melo@gmail.com>
2022-05-19 23:30:21 -0300

    Tech Page

--- src/Routes/About/About.jsx
+++ src/Routes/About/About.jsx
@@ -6,6 +6,7 @@ function About() {
         <div className="main--About">
             <article>
                 <h1>About...</h1>
+                <hr />
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores sit laboriosam quo repellat praesentium molestiae dolorem soluta reiciendis rem natus omnis error, dolore consequuntur aspernatur eligendi odit vel, accusantium alias!</p>
             </article>
         </div>
--- src/Routes/Technologies/Technologies.jsx
+++ src/Routes/Technologies/Technologies.jsx
@@ -1,10 +1,22 @@
 import * as React from 'react'
 import './Technologies.scss'
 
+import { SiHtml5, SiCss3, SiJavascript, SiGit, SiTypescript, SiReact, SiSass, SiMongodb } from 'react-icons/si'
+
+
 function Technologies() {
     return (
         <div className="main--Technologies">
-
+            <article>
+                <SiHtml5 className='HTML' />
+                <SiCss3 className='CSS3' />
+                <SiJavascript className='JS' />
+                <SiMongodb className='MONGO' />
+                <SiGit className='GIT' />
+                <SiTypescript className='TS' />
+                <SiReact className='REACT' />
+                <SiSass className='SASS' />
+            </article>
         </div>
     )
 }
