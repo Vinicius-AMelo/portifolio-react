0521432ba850a60539ce42e3a8660a886c947196
Vinícius Melo <vinicius.apol.melo@gmail.com>
2022-05-24 01:04:49 -0300

    loading sharingan

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
 import Nav from './components/Nav/Nav'
 import Content from './components/Content/Content'
 import { HashRouter } from 'react-router-dom'
 
+import Fundo from './images/fundo.png'
+import Foto from './images/react-js.png'
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false)
+  }, 4000)
   return (
     <div className='App'>
       <HashRouter>
-        <Nav />
-        <Content />
+        {!loading && <Nav />}
+        {loading && <div className='fundo'>
+          <img className='sharin' src={Fundo} alt="" />
+          <div className='eye'></div>
+          <img className='mange' src={Foto} alt="foto" />
+          <img className='kyo' src={Foto} alt="foto" />
+        </div>}
+        {!loading && <Content />}
       </HashRouter>
     </div>
   )
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,12 +3,14 @@ import { NavLink } from 'react-router-dom'
 import { FaHome, FaUserAlt, FaTools, FaLaptopCode, FaEnvelope } from 'react-icons/fa'
 import './Nav.scss'
 
-import Foto from '../../images/fundo.jpg'
+import Foto from '../../images/react-js.png'
+import black from '../../images/icon-1.1s-200px.png'
 
 function Nav() {
     return (
         <nav className='main--Nav'>
             <div className='foto'>
+                <img className='presha' src={black} alt="" />
                 <img src={Foto} alt="foto" />
             </div>
             <ul>
