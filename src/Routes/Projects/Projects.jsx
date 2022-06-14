01d9f5b37519f541172d4382c286323c42ec2785
Vinícius Melo <vinicius.apol.melo@gmail.com>
2022-06-14 20:00:53 -0300

    :iphone: fix: Responsividade

--- src/Routes/Projects/Projects.jsx
+++ src/Routes/Projects/Projects.jsx
@@ -6,6 +6,8 @@ import Layout from './layout/Layout';
 import Tenzies from '../../images/Tenzies.jpg'
 import Cadastro from '../../images/CadastroFullStack.jpg'
 import TaskList from '../../images/TaskList.jpg'
+import Weather from '../../images/Weather-App.jpeg'
+import Pokedex from '../../images/Pokedex.jpeg'
 
 
 
@@ -14,7 +16,9 @@ function Projects() {
         <section className='main--Projects'>
             <Layout linkGitHub="https://github.com/Vinicius-AMelo/dice-roll-game" linkPages="https://vinicius-amelo.github.io/dice-roll-game/" img={Tenzies} techs={['React', 'JavaScript']} projectName='Tenzie Game' projectDescription='Jogo de rolagem de dados, Projeto realizado seguindo o módulo React Basics do Curso The Frontend Developer Career Path da plataforma Scrimba.' />
             <Layout linkGitHub="https://github.com/Vinicius-AMelo/cadastro-fullstack" linkPages="https://cadastro-fullstack.vercel.app/" img={Cadastro} techs={['React', 'NextJS', 'TailwindCSS', 'TypeScript', 'Firebase']} projectName='Cadastro FullStack' projectDescription='Projeto de Cadastro simples de usuários, usando NextJS e o banco de dados do Firebase como backend, o cadastro é feito com o nome e idade, e o ID é retornado pelo próprio Firestore.' />
-            <Layout linkGitHub="https://github.com/Vinicius-AMelo/task-list" linkPages="https://vinicius-amelo.github.io/task-list/" img={TaskList} techs={['React', 'JavaScript', 'ESLINT', 'LocalStorage']} projectName='Task List' projectDescription='Projeto pessoal que fiz para praticar os fundamentos do React que tinha aprendido até aquele momento' />
+            <Layout linkGitHub="https://github.com/Vinicius-AMelo/task-list" linkPages="https://vinicius-amelo.github.io/task-list/" img={TaskList} techs={['React', 'JavaScript', 'ESLINT', 'LocalStorage']} projectName='Task List' projectDescription='Projeto pessoal que fiz para praticar os fundamentos do React que tinha aprendido até aquele momento.' />
+            <Layout linkGitHub="https://github.com/Vinicius-AMelo/weather-app" linkPages="https://vinicius-amelo.github.io/weather-app/" img={Weather} techs={['React', 'TypeScript', 'ESLINT', 'Prettier', 'Consumo de API']} projectName='Weather App' projectDescription='Primeiro projeto consumindo uma API, usei como aprendizado do que estava vendo no momento, no início dos meus estudos em React com TypeScript.' />
+            <Layout linkGitHub="https://github.com/Vinicius-AMelo/pokedex" linkPages="https://pokedex-murex-sigma.vercel.app/" img={Pokedex} techs={['React', 'TypeScript', 'Next.JS', 'ContextAPI', 'SASS', 'Consumo de API']} projectName='Pokedex' projectDescription='Meu projeto principal até então, consumindo a POKEAPI para criar uma pokedex com os 10 pokemons mais legais (minha opinião), com animações de ligar e desligar a tela e com o menu retrátil.' />
 
         </section>
     )
--- src/Routes/Projects/layout/Layout.jsx
+++ src/Routes/Projects/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../../../sass/Layout.scss'
 
-import { FaLaptop, FaGithub } from 'react-icons/fa'
+import { FaAtlas, FaGithub } from 'react-icons/fa'
 import { IoCloseSharp } from 'react-icons/io5'
 
 
@@ -10,7 +10,7 @@ function Layout({ linkPages, linkGitHub, img, techs, projectName, projectDescrip
     const [active, setActive] = useState(false);
 
     function showWindow() {
-        setActive(!active)
+        setActive(true)
     }
 
     function closeWindow() {
@@ -20,41 +20,40 @@ function Layout({ linkPages, linkGitHub, img, techs, projectName, projectDescrip
 
     return (
         <div className='main--Layout'>
-            {active && <div className='cc'><IoCloseSharp onClick={closeWindow} className='closeWindow' /></div>}
+            {active && <div className='shadow'></div>}
             <div onClick={showWindow} className='Layout' >
                 <img src={img} alt="z" />
-
-                {active &&
-
-
-                    <div onClick={showWindow} className='active' >
-                        <img src={img} alt="z" />
-
-
-                        <div className='infos'>
-                            <div className='techs'>
-                                {techs.map(tech => <p>{tech}</p>)}
-                            </div>
-                            <h1>{projectName}</h1>
-                            <p>{projectDescription}</p>
-
-
-                            <div className='buttons'>
-
-                                <a target='_blank'
-                                    rel='noreferrer'
-                                    href={linkGitHub}>
-                                    <FaGithub />
-                                </a>
-                                <a target='_blank'
-                                    rel='noreferrer'
-                                    href={linkPages}>
-                                    <FaLaptop />
-                                </a>
-                            </div>
-                        </div>
-                    </div>}
             </div >
+            {active &&
+                <div className='active' >
+                    <IoCloseSharp onClick={closeWindow} className='closeWindow' />
+                    <img src={img} alt="z" />
+
+
+                    <div className='infos'>
+                        <div className='techs'>
+                            {techs.map(tech => <p>{tech}</p>)}
+                        </div>
+                        <h1>{projectName}</h1>
+                        <p>{projectDescription}</p>
+
+
+                        <div className='buttons'>
+
+                            <a target='_blank'
+                                rel='noreferrer'
+                                href={linkGitHub}>
+                                <FaGithub />
+                            </a>
+                            <a target='_blank'
+                                rel='noreferrer'
+                                href={linkPages}>
+                                <FaAtlas />
+                            </a>
+                        </div>
+                    </div>
+                </div>}
+
         </div>
     );
 }
